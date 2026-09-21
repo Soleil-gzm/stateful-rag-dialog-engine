0cb709d6d6d56aa174551e516e0c029327f3baea
Soleil <guozimengG@outlook.com>
2026-09-21 05:26:27 +0000

    rebuild: 重组架构 3

--- src/stateful_rag_dialog_engine/run_module.py
+++ src/stateful_rag_dialog_engine/run_module.py
@@ -8,9 +8,9 @@ Parameter configuration in 'args/qwen.json'
 
 import argparse
 
-from .engine.arguments.arguments import CustomizedArguments
-from .engine.component.template import template_dict
-from .engine.component.structure_frontend_v2 import Pathway, Module
+from .arguments.arguments import CustomizedArguments
+from .component.template import template_dict
+from .component.structure_frontend_v2 import Pathway, Module
 
 from langchain_huggingface import HuggingFaceEmbeddings
 
